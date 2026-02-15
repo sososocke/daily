77d147cddf80629e555289fe14c0a21c8b7c3fda
Linda <linda-264@web.de>
2026-02-15 22:36:20 +0100

    home form first draft

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonDatetime } from '@ionic/angular/standalone';
+import { IonRange } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonDatetime, IonRange],
 })
 export class HomePage {
   constructor() {}
